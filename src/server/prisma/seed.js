104e8c588be9b5ef1113f57ecfdfdfa067fec65d
Pauline Pan <pan_pauline@yahoo.com>
2023-11-27 16:58:24 -0500

    Add seed.js code

--- src/server/prisma/seed.js
+++ src/server/prisma/seed.js
@@ -1,21 +1,44 @@
 const prisma = require("../prisma");
+//import the csv file
 
-/** Seeds the database with a user and some tasks */
+/** Seeds the database with a user*/
 const seed = async () => {
-  await prisma.user.create({
-    data: {
-      username: "foo",
-      password: "bar",
-      tasks: {
-        create: [
-          { description: "task 1" },
-          { description: "task 2" },
-          { description: "task 3" },
-        ],
-      },
-    },
-  });
-};
+  //loop through the csv file for each item
+
+  //create plant data with looped items
+  //data: {
+  //    latin
+  //    common
+  //    category
+  //    tempmax
+  //    tempmin
+  //    ideallight
+  //    toleratedlight
+  //    watering
+  //    image
+  //    myplants
+  //    notes
+  //    reviews
+  //    stores
+  //    spare1
+  //    spare2
+  //}
+
+//commented out the previous code, can be deleted after start coding
+//   await prisma.user.create({
+//     data: {
+//       username: "foo",
+//       password: "bar",
+//       tasks: {
+//         create: [
+//           { description: "task 1" },
+//           { description: "task 2" },
+//           { description: "task 3" },
+//         ],
+//       },
+//     },
+//   });
+// };
 
 seed()
   .then(async () => await prisma.$disconnect())
