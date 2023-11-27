a4873dd51122765a9e2c96b1eaaa35ee19578fc7
CourtneyATX <courtney.mcguire.tx@gmail.com>
2023-11-27 15:08:48 -0600

    plantSlice pseudo

---
+++ src/client/features/plants/plantSlice.js
@@ -0,0 +1,21 @@
+// import api
+
+// set plantsApi as api.injectEndpoints
+// endpoints builder
+//   getPlants query
+//      /plants
+//      tags "plants"
+//   getPlant query
+//      /plants/${id}
+//      tags "plants"
+//   createReview mutation     
+// **this is creating a new review, but editing an existing plant by id
+//      /plants/${plant.id}
+//      method "PUT"
+//      body: review
+
+// export const 
+// usegetPlantsQuery
+// useGetPlantQuery
+// useCreateReviewMutation
+// = plantsApi
