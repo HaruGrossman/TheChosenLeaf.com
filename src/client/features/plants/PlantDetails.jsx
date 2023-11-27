5f3302630469fd1f7b84e25e6b62c824e616c13e
CourtneyATX <courtney.mcguire.tx@gmail.com>
2023-11-27 13:03:38 -0600

    beginning phase of plantdetails pseudocode

---
+++ src/client/features/plants/PlantDetails.jsx
@@ -0,0 +1,44 @@
+// import everything needed
+
+// useGetPlantQuery from plantSlice
+// import userParams and useNavigate from react router dom
+// import useState from react
+// import css
+
+// export default function PlantDetails()
+
+// set navigate as useNavigate
+// set id as useParams
+// set data, isLoading, isError as useGetPlantQuery(id)
+
+// set isLoading to return
+// set undefined data to navigate home
+
+// validate login
+
+// arrow function returnNavigate returns to search list
+
+// arrow function storeNavigate takes user to maps/nearme page
+// ***Q will this onClick need to store params to be exported to the maps page?
+
+// arrow function createReview returns a popup window to create a review
+// ***Q will this need to be added to the plant API to save the review under each plant? 
+
+// return
+// div
+// if there is data display:
+// image
+// Common Name : common name
+// Latin Name : latin
+// Family : family
+// Ideal Light : ideallight
+// Tolerated Light : toleratedlight
+// Watering : watering
+// Temperature : tempmin - tempmax
+// Category : category
+// Reviews : <li> list of ReviewCard(s) (review and rating) </li>
+
+// add to favorite button onClick = 
+// store near me button onClick = storeNavigate
+// return button onClick = returnNavigate
+// review button onClick = createReview
