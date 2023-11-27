9a9787be8bdf28bd2c6a14e40d7310e70d85a2a8
HaruGrossman <dogtaboodog@yahoo.com>
2023-11-27 16:03:34 -0600

    Footer Pseudo code

--- src/client/layout/Footer.jsx
+++ src/client/layout/Footer.jsx
@@ -9,6 +9,5 @@
 // Courtney Snyder - used as a hyperlink to linkedin https://www.linkedin.com/in/courtneynsnyder/
 // Haru Grossman - used as a hyperlink to linkedin https://www.linkedin.com/in/harugrossman/
 
-
 // styling - needs to have this stuck at the bottom of the window/screen depending on the size of the window
 //         - all images equally spaced
