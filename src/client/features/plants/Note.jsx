d2bb5630e374525c53cb120b7f931dee1655574b
HaruGrossman <dogtaboodog@yahoo.com>
2023-11-27 15:35:24 -0600

    Note Pseudo code

---
+++ src/client/features/plants/Note.jsx
@@ -0,0 +1,38 @@
+//import react
+//import useState
+
+
+//~ function for rendering our createNoteForm
+// window to pop up on screen
+
+// import field for text //in css need to set dimensions for this box for all popup windows
+
+// "x" button for closing window
+///onclick close window
+////error if text!=== 0 {return "Are you sure you want to close without saving note?"}
+
+// "Save Note" button to add 
+///onclick save note and close window
+////error if text === 0 {return "There is no note to save"}
+
+
+
+//~ function for rendering our editNoteForm
+// window to pop up on screen
+
+// input field for text
+// text.value = text[selected note id] ; This will be the previous saved Note
+
+// "x" button for closing window
+///onclick close window
+////error if no change in text {return "No changes are saved"}
+
+// "Edit Note" button to add 
+///onclick update state, delete previous data and close popup window
+
+
+
+//getElement by id createNoteBTN
+//onclick createNoteBTN(createNoteForm)
+//getElement by id editBTN
+//onclick editBTN(editNoteForm)
