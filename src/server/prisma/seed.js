36584aa710f8c0b3722331e606716aad89a67ace
Pauline Pan <pan_pauline@yahoo.com>
2023-11-28 14:32:20 -0500

    Add data models

--- src/server/prisma/seed.js
+++ src/server/prisma/seed.js
@@ -3,26 +3,26 @@ const prisma = require("../prisma");
 
 /** Seeds the database with a user*/
 // const seed = async () => {
-  //loop through the csv file for each item
+//loop through the csv file for each item
 
-  //create plant data with looped items
-  //data: {
-  //    latin
-  //    common
-  //    category
-  //    tempmax
-  //    tempmin
-  //    ideallight
-  //    toleratedlight
-  //    watering
-  //    image
-  //    myplants
-  //    notes
-  //    reviews
-  //    stores
-  //    spare1
-  //    spare2
-  //}
+//create plant data with looped items
+//data: {
+//    latin
+//    common
+//    category
+//    tempmax
+//    tempmin
+//    ideallight
+//    toleratedlight
+//    watering
+//    image
+//    myplants
+//    notes
+//    reviews
+//    stores
+//    spare1
+//    spare2
+//}
 
 //commented out the previous code, can be deleted after start coding
 //   await prisma.user.create({
