a0a8da645f45c2117676a96cf3364193fe1ad9be
CourtneyATX <courtney.mcguire.tx@gmail.com>
2023-11-28 09:38:18 -0600

    added all notes, all favorited plants, edit note

--- src/server/api/plants.js
+++ src/server/api/plants.js
@@ -16,9 +16,19 @@
 // validates that user is logged in
 // modifies a plant based on id to add the review
 
+// sends all favorited plants
+// finds many where user exists and favorite true exists
+
+// sends all notes
+// finds many notes
+
 // creates a new note in the account page
 // validates user is logged in
 // requires a id and user info
+
+// puts an edit on a note in the account page
+// validates user is logged in
+// requires an id and user info
 
 // deletes a note in account page
 // validates user is logged in
