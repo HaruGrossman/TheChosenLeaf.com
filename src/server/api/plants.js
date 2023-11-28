916e09d7ac4f41caace30c8bd65b6dddc3aafeaa
CourtneyATX <courtney.mcguire.tx@gmail.com>
2023-11-28 08:49:09 -0600

    plants server routing pseudocode

---
+++ src/server/api/plants.js
@@ -0,0 +1,25 @@
+// set { ServerError } = require ("../errors");
+// set prisma to require prisma
+// set router a require ("express").Router()
+// module.exports = router
+
+// sends all plants 
+// finds many plants 
+
+// send a single plant
+// finds a unique plant where the params match the plant id
+
+// must be logged in to access reviews/notes
+// validates that user is logged in
+
+// puts a new review within a plant and sends it
+// validates that user is logged in
+// modifies a plant based on id to add the review
+
+// creates a new note in the account page
+// validates user is logged in
+// requires a id and user info
+
+// deletes a note in account page
+// validates user is logged in
+// finds unique where id exists and user info
