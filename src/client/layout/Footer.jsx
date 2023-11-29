1e44cdbbcce51a70fd8833abd7f718ef1fdbd6c7
CourtneyATX <courtney.mcguire.tx@gmail.com>
2023-11-29 14:24:46 -0600

    Added beginning stages of footer

--- src/client/layout/Footer.jsx
+++ src/client/layout/Footer.jsx
@@ -1,8 +1,18 @@
-// icon to instagram
-
-// icon to facebook
-
-// icon to twitter @TheChosenPlant
+export default function Footer(){
+    return(
+        <footer className="footer">
+            <a href="www.instagram.com/thechosenplant">
+                <img border="0" alt="Instagram" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/600px-Instagram_icon.png?20200512141346" width="70" height="70"/>
+            </a>
+            <a href="www.facebook.com/thechosenplant">
+                <img border="0" alt="Facebook" src="https://1000logos.net/wp-content/uploads/2017/02/Facebook-Logosu.png" width="125" height="75"/>
+            </a>
+            <a href="www.twitter.com/thechosenplant">
+                <img border="0" alt="Twitter" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Logo_of_Twitter.svg/512px-Logo_of_Twitter.svg.png?20220821125553" width="70" height="70"/>
+            </a>
+        </footer>
+    )
+}
 
 // hamburger list of creators clickable
 // Pauline Pan - used as a hyperlink to linkedin https://www.linkedin.com/in/pauline-pan/
