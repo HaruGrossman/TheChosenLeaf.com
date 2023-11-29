98af89232f7706e7c527b2066a66d0d28755e6d1
CourtneyATX <courtney.mcguire.tx@gmail.com>
2023-11-29 14:59:59 -0600

    unfavorite a plant/card details

--- src/client/features/plants/PlantDetails.jsx
+++ src/client/features/plants/PlantDetails.jsx
@@ -8,7 +8,9 @@ import Reviews from "./Review";
 
 export default function Details(){
 
+// validate login
     const token = useSelector(selectToken);
+
     const navigate = useNavigate();
 
     const { id } = useParams();
@@ -20,6 +22,12 @@ export default function Details(){
         await favoritePlant(plant.id);
     };
 
+    const [unfavoritePlant, { isLoading: isUnfavoriting }] = useUnfavoritePlantMutation();
+    tryUnfavoritePlant = async (evt) => {
+        evt.preventDefault();
+        await unfavoritePlant(plant.id);
+    }
+
     const [reviewPlant, { isLoading: isReviewing }] = usePlantReviewMutation();
     tryReviewPlant = async (evt) => {
         evt.preventDefault();
@@ -30,21 +38,28 @@ export default function Details(){
     <p>Loading...</p>
     ) : (
     <main className="plant-details">
-        <h1>{plant.common}</h1>
-        
+        <img src={plant.image} />
+        <h1>Common Name:{plant.common}</h1>
+        <h2>Latin Name:{plant.latin}</h2>
+        <h2>Ideal Light:{plant.ideallight}</h2>
+        <h2>Tolerated Light:{plant.toleratedlight}</h2>
+        <h2>Watering:{plant.watering}</h2>
+        <h2>Temperature:{plant.tempmin}-{plant.tempmax}</h2>
+        <h2>Category:{plant.category} </h2>
+        { token &&
+            (book.favorite === false ? (
+                <form onSubmit={tryFavoritePlant}>
+                    <button>{isFavoriting ? "Hang tight..." : "❤️" }</button>
+                </form>
+            ) : (
+                <form onSubmit={tryUnfavoritePlant}>
+                    <button>{isUnfavoriting ? "Hang tight..." : "🤍" }</button>
+                </form>
+            ))
+        };
         <Reviews />
-    </div>)
+    </main>)
 }
-
-
-// set navigate as useNavigate
-// set id as useParams
-// set data, isLoading, isError as useGetPlantQuery(id)
-
-// set isLoading to return
-// set undefined data to navigate home
-
-// validate login
 
 // arrow function returnNavigate returns to search list
 
@@ -54,17 +69,6 @@ export default function Details(){
 // arrow function createReview returns a popup window to create a review
 // ***Q will this need to be added to the plant API to save the review under each plant? 
 
-// return
-// div
-// if there is data display:
-// image
-// Common Name : common name
-// Latin Name : latin
-// Ideal Light : ideallight
-// Tolerated Light : toleratedlight
-// Watering : watering
-// Temperature : tempmin - tempmax
-// Category : category
 // Reviews : <li> list of ReviewCard(s) (review and rating) </li>
 
 // add to favorite button onClick = 
