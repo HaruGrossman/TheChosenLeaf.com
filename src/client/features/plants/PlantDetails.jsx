dee089539c2eb42a07fdb750daef96cb6a288502
HaruGrossman <dogtaboodog@yahoo.com>
2023-11-30 10:08:48 -0600

    PlantDetails.jsx page will all Courtney's code

--- src/client/features/plants/PlantDetails.jsx
+++ src/client/features/plants/PlantDetails.jsx
@@ -1,28 +1,53 @@
-// import everything needed
-
+import { UseSelector, useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
+// import {selectToken} from authslice
 // useGetPlantQuery from plantSlice
-// import userParams and useNavigate from react router dom
-// import useState from react
 // import css
 import Reviews from "./Review";
 
-export default function Details(){
-    return (
-    <div>
-        <h1>The Chosen Plant's Plant Details</h1>
-        <Reviews />
-    </div>)
+export default function Details() {
+    // validate login 
+    const token = useSelector(selectToken);
+    const navigate = useNavigate();
+    const { id } = useParams();
+    const { data: plant, isLoading } = useGetPlantQuery(id);
+    const [favoritePlant, { isLoading: isFavoriting }] = useFavoritePlantMutation();
+    tryFavoritePlant = async (event) => {
+        event.preventDefault();
+        await favoritePlant(plant.id);
+    };
+    const [reviewPlant, { isLoading: isReviewing }] = usePlantReviewMutation();
+    tryReviewPlant = async (event) => {
+        event.preventDefault();
+        await reviewPlant(plant.id);
+    };
+    return isLoading ? (
+        <p>Please wait Loading...</p>
+    ) : (
+        <main className="plant-details">
+            <img src={plant.image} />
+            <h1>Common Name:{plant.common}</h1>
+            <h2>Latin Name:{plant.latin}</h2>
+            <h2>Ideal Light:{plant.ideallight}</h2>
+            <h2>Tolerated Light:{plant.toleratedlight}</h2>
+            <h2>Watering:{plant.watering}</h2>
+            <h2>Temperature:{plant.tempmin}-{plant.tempmax}</h2>
+            <h2>Category:{plant.category} </h2>
+            {token &&
+                (book.favorite === false ? (
+                    <form onSubmit={tryFavoritePlant}>
+                        <button>{isFavoriting ? "Hang tight..." : "❤️"}</button>
+                    </form>
+                ) : (
+                    <form onSubmit={tryUnfavoritePlant}>
+                        <button>{isUnfavoriting ? "Hang tight..." : "🤍"}</button>
+                    </form>
+                ))
+            };
+            <Reviews />
+        </main>
+    )
 }
-
-
-// set navigate as useNavigate
-// set id as useParams
-// set data, isLoading, isError as useGetPlantQuery(id)
-
-// set isLoading to return
-// set undefined data to navigate home
-
-// validate login
 
 // arrow function returnNavigate returns to search list
 
@@ -32,17 +57,6 @@ export default function Details(){
 // arrow function createReview returns a popup window to create a review
 // ***Q will this need to be added to the plant API to save the review under each plant? 
 
-// return
-// div
-// if there is data display:
-// image
-// Common Name : common name
-// Latin Name : latin
-// Ideal Light : ideallight
-// Tolerated Light : toleratedlight
-// Watering : watering
-// Temperature : tempmin - tempmax
-// Category : category
 // Reviews : <li> list of ReviewCard(s) (review and rating) </li>
 
 // add to favorite button onClick = 
