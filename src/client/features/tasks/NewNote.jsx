f4fb76b933dc3e46504c5759f4a85a15e704be15
CourtneyATX <courtney.mcguire.tx@gmail.com>
2023-12-04 09:39:55 -0600

    new note saves to db

---
+++ src/client/features/tasks/NewNote.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+// import { useCreateTaskMutation } from "./taskSlice";
+import { useCreateNoteMutation } from "../../store/Notes";
+
+/** Form for creating new tasks */
+export default function NewNote() {
+  const [note, setNote] = useState("");
+  const [createNote] = useCreateNoteMutation();
+
+  const create = async (evt) => {
+    evt.preventDefault();
+    createNote({ note });
+  };
+
+  return (
+    <form onSubmit={create}>
+      <input
+        type="text"
+        value={note}
+        onChange={(e) => setNote(e.target.value)}
+        required
+      />
+      <button>Create</button>
+    </form>
+  );
+}
