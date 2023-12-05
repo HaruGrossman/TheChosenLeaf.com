39dc6f128c3e51eba9c8c74615db9ed32d8250f6
CourtneyATX <courtney.mcguire.tx@gmail.com>
2023-12-05 15:21:33 -0600

    edit note popup

--- src/client/features/notes/Note.jsx
+++ src/client/features/notes/Note.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useDeleteNoteMutation, useEditNoteMutation } from "../../store/Notes";
+import Popup from "../components/Popup";
 
 //  function for rendering our createNoteForm  // window to pop up on screen
 export default function Note ({ note }) {
@@ -7,12 +8,14 @@ export default function Note ({ note }) {
     const [editNote] = useEditNoteMutation();
     const [deleteNote] = useDeleteNoteMutation();
     
-    const [editedNote, setEditedNote]= useState(note.note);
+    const [editedNote, setEditedNote] = useState(note.editedNote);
+    const [buttonPopup, setButtonPopup] = useState(false);
 
     // save edited note
     const save = async (evt) => {
+        id = note.id;
         evt.preventDefault();
-        editNote({ ...note, editedNote});
+        editNote({ ...id, editedNote });
     }
 
     // delete note
@@ -21,11 +24,11 @@ export default function Note ({ note }) {
         deleteNote(note.id);
     };
 
-    const onDisplay = async (evt) => {
-        evt.preventDefault();
-        
-        return (
-            <li>
+    return (
+        <li>
+            <h3>{note.note}</h3>
+            <button onClick={() => setButtonPopup(true)}>Edit Note</button>
+            <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
             <form onSubmit={save}>
                 <input 
                     type="text" 
@@ -35,41 +38,11 @@ export default function Note ({ note }) {
                 />
                 <button>Save Changes</button>
                 <button onClick={onDelete}> 
-                Delete 
+                🞪 
                 </button>
             </form>
-        </li>
-        )
-    }
+            </Popup>
+    </li>
 
-    return (
-        <>
-        <h3>{note.note}</h3>
-        <button onClick={onDisplay}>Edit Note</button>
-        </>
     )
 }
-
-
-    // const select = (e) => {
-    //     e.preventDefault();
-    //     const id = note.id;
-    //     selectNote({id});
-    //     console.log(selectedNote);
-    // };
-
-    // const showNote = async (evt) => {
-    //     const hidden = evt.target.style.visibility = "hidden";
-    // ul.addEventListener("click", hide, false);
-    // }
-
-    // const hideNote = async (evt) => {
-    //     const visible = evt.target.style.visibility = "visible";
-    //     ul.addEventListener("click", hide, true);
-    // }
-
-    // function toggleNote(id){
-        // const target = document.getElementById(id),
-        // visibility = target.style.visibility;
-        // target.style.visibility = visibility && visibility == 'visibile' ? 'hidden' : 'visibile';
-    // }
