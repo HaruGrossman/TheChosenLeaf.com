174e4929202b75d67cfa240a6a6e328fa2c828c5
Haru Grossman <127241442+HaruGrossman@users.noreply.github.com>
2023-12-14 11:27:34 -0600

    Update seed.js

--- src/server/prisma/seed.js
+++ src/server/prisma/seed.js
@@ -2,7 +2,7 @@ const prisma = require("../prisma");
 const plantData = require("./plantData");
  //can be commented after initial seed since username is unique cannot be created again
 const seed = async () => {
-  await prisma.user.create({
+  await prisma.user.upsert({
     data: {
       username: "panda",
       password: "123",
